d128d7ab1ed24792566b1706424b4735107d038a
poncema4 <marco.ponce@student.shu.edu>
2025-04-30 13:51:03 -0400

    more changes to the chat_bot folder prefix

--- Chat_Bot/chat_bot.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-:: Get the directory where this batch file is located
-set SCRIPT_DIR=%~dp0
-
-:: Run the Python script from its original directory
-cd /d "%SCRIPT_DIR%"
-python chat_bot.py
